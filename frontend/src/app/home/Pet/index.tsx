c7bf780c3f7e851d59f5a884bebf59c3f3e6ef9f
diem4894 <thayselakdim@hotmail.com>
2025-02-05 12:05:34 +0200

    frontend code

---
+++ frontend/src/app/home/Pet/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+import { Actions, PetAction } from "./Actions";
+import { PetDetails } from "./Details";
+import { PetImage } from "./Image";
+import { Summary } from "./Summary";
+import { AptogotchiCollection } from "@/components/AptogotchiCollection";
+
+export interface Pet {
+  name: string;
+  birthday: number;
+  energy_points: number;
+  parts: PetParts;
+}
+
+export interface PetParts {
+  body: number;
+  ear: number;
+  face: number;
+}
+
+export const DEFAULT_PET = {
+  name: "Unknown",
+  energy_points: 0,
+  parts: {
+    body: 0,
+    ear: 0,
+    face: 0,
+  },
+};
+
+interface PetProps {
+  pet: Pet;
+  setPet: Dispatch<SetStateAction<Pet | undefined>>;
+}
+
+export function Pet({ pet, setPet }: PetProps) {
+  const [selectedAction, setSelectedAction] = useState<PetAction>("play");
+
+  return (
+    <div className="flex flex-col self-center m-10">
+      <div className="flex flex-row self-center gap-12">
+        <div className="flex flex-col gap-4 w-[360px]">
+          <PetImage
+            selectedAction={selectedAction}
+            petParts={pet.parts}
+            avatarStyle
+          />
+          <PetDetails pet={pet} setPet={setPet} />
+        </div>
+        <div className="flex flex-col gap-8 w-[680px] h-full">
+          <Actions
+            selectedAction={selectedAction}
+            setSelectedAction={setSelectedAction}
+            setPet={setPet}
+            pet={pet}
+          />
+          <Summary pet={pet} />
+        </div>
+      </div>
+      <AptogotchiCollection />
+    </div>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,60 @@
+import dynamic from "next/dynamic";
+import { Body } from "./home/Body";
+import { PropsWithChildren } from "react";
+
+const FixedSizeWrapper = ({ children }: PropsWithChildren) => {
+  const fixedStyle = {
+    width: "1200px",
+    height: "800px",
+    border: "6px solid",
+    margin: "auto",
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={fixedStyle}>{children}</div>
+    </div>
+  );
+};
+
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <FixedSizeWrapper>
+        <Header />
+        <Body />
+      </FixedSizeWrapper>
+    </main>
+  );
+}
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-10 flex justify-between items-center px-6 py-4 bg-gradient-to-r from-orange-300 via-orange-400 to-red-400 shadow-md w-full gap-2">
+      <h1 className="text-2xl">Aptogotchi</h1>
+      <WalletButtons />
+    </header>
+  );
+}
+
+const WalletButtons = dynamic(
+  async () => {
+    const { WalletButtons } = await import("@/components/WalletButtons");
+    return { default: WalletButtons };
+  },
+  {
+    loading: () => (
+      <div className="nes-btn is-primary opacity-50 cursor-not-allowed">
+        Loading...
+      </div>
+    ),
+    ssr: false,
+  }
+);
